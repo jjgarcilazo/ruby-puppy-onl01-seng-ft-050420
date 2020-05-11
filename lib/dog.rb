93f152274f68bbc1675ea780f17f5f967f7f738e
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-11 19:49:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Add your code here
+class Dog
+  attr_accressor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear_all
+  end
+
+  def self.all
+    @@all.each do |d|
+      puts d.name
+    end
+  end
+end
